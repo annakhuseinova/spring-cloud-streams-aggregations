6e3db6958eeadadac1a6974c0295bf628ae44c05
annakhuseinova <annakhuseinova@yandex.ru>
2021-06-20 13:22:44 +0300

    More code

---
+++ src/main/java/com/annakhuseinova/springcloudstreamsaggregations/SpringCloudStreamsAggregationsApplication.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.springcloudstreamsaggregations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamsAggregationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStreamsAggregationsApplication.class, args);
+    }
+
+}
